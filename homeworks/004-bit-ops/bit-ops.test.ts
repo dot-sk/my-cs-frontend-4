143673a13bf8da60a41cf9109160ea4c7f38b40b
Сергей Хохлачев <s.s.khokhlachev@tinkoff.ru>
2026-03-31 01:56:04 +0400

    Добавляет 4 домашку

---
+++ homeworks/004-bit-ops/bit-ops.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, test } from "bun:test";
+import { cyclicLeftShift, cyclicRightShift } from "./bit-ops";
+
+describe("004 Bit operations", () => {
+  test("cyclicLeftShift должен сдвигать биты влево циклически", () => {
+    expect(cyclicLeftShift(0b10000000_00000000_00000000_00000001, 1)).toBe(
+      0b00000000_00000000_00000000_00000011,
+    );
+    expect(cyclicLeftShift(0b10000000_00000000_00000000_00000001, 2)).toBe(
+      0b00000000_00000000_00000000_00000110,
+    );
+    expect(cyclicLeftShift(0b00000000_00000000_00000000_00000011, 2)).toBe(
+      0b00000000_00000000_00000000_00001100,
+    );
+  });
+
+  test("cyclicRightShift должен сдвигать биты вправо циклически", () => {
+    expect(cyclicRightShift(0b10000000_00000000_00000000_00000001, 1)).toBe(
+      0b11000000_00000000_00000000_00000000,
+    );
+    expect(cyclicRightShift(0b11000000_00000000_00000000_00000000, 2)).toBe(
+      0b00110000_00000000_00000000_00000000,
+    );
+  });
+
+  test("Сдвиг на 0 НЕ должен изменять число", () => {
+    expect(cyclicLeftShift(0b1011, 0)).toBe(0b1011);
+    expect(cyclicRightShift(0b1011, 0)).toBe(0b1011);
+  });
+
+  test("Сдвиг влево на n/2 и сдвиг вправо на n/2 дают одинаковый результат (при чётном n)", () => {
+    expect(cyclicLeftShift(0b1011, 16)).toBe(cyclicRightShift(0b1011, 16));
+  });
+});
---
+++ homeworks/004-bit-ops/bit-ops.ts
@@ -0,0 +1,20 @@
+export function cyclicLeftShift(num: number, shift: number): number {
+  // потому что числа становятся int32 при битовых операциях
+  if (shift % 32 === 0) {
+    return num;
+  }
+
+  // сдвигаем число влево на shift и добавляем к нему результат логического сдвига числа вправо на (32 - shift)
+  return (num << shift) | (num >>> (32 - shift));
+}
+
+export function cyclicRightShift(num: number, shift: number): number {
+  // потому что числа становятся int32 при битовых операциях
+  if (shift % 32 === 0) {
+    return num;
+  }
+
+  // сдвигаем число логически вправо на shift и добавляем к нему результат сдвига числа влево на (32 - shift)
+  // >>> — вызывает ToUint32 → старший бит = просто бит => для корректного сравнения с float64 числами
+  return ((num >>> shift) | (num << (32 - shift))) >>> 0;
+}
